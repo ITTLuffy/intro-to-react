e146156137976b22d778b2d1559dafb06182fc05
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-04-09 12:37:10 +0200

    Refactor component imports and add new components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client' // Importa il pacchetto react-dom/client --> funzione che permette di creare un root
 import './index.css'
 import App from './App.tsx'
 
-const elementoDOMDiMounting = document.getElementById('root');
-const root = createRoot(elementoDOMDiMounting!);
+const elementoDOMDiMounting = document.getElementById('root'); // Se non esiste l'elemento DOM, mostra un errore
+const root = createRoot(elementoDOMDiMounting!); // Crea un root React e lo collega all'elemento DOM di mounting
 
 root.render(
   <StrictMode>
@@ -13,3 +13,35 @@ root.render(
 )
 
 // logica di base di react
+
+
+function MyButton() {
+  return (
+    <button>
+      Click me!
+    </button>
+  )
+}
+
+root.render(
+  <StrictMode>
+    <MyButton />
+  </StrictMode>,
+)
+
+function MyApp() {
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+        <p>This is my first React app</p>
+      </div>
+      <MyButton />
+    </>
+  )
+}
+
+
+MyButton.displayName = 'MyButton' // Imposta il nome del componente per il debug
+// Il nome del componente viene visualizzato negli strumenti di sviluppo React
+// Puoi anche usare un nome di funzione per il debug
